2f8aa63bcb0e24d4b2a825b1dba4baa2859ef903
linarus85 <linarus85@gmail.com>
2022-08-20 15:44:39 +0300

    fix

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -21,7 +21,7 @@ export const PostItem = ({ post }) => {
                 >
                     {post.imgUrl && (
                         <img
-                            src={`http://localhost:3001/${post.imgUrl}`}
+                            src={`${process.env.REACT_APP_API_URL}/${post.imgUrl}`}
                             alt='img'
                             className='object-cover w-full'
                         />
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -99,7 +99,7 @@ export const PostPage = () => {
                         >
                             {post?.imgUrl && (
                                 <img
-                                    src={`http://localhost:3001/${post.imgUrl}`}
+                                    src={`${process.env.REACT_APP_API_URL}/${post.imgUrl}`}
                                     alt='img'
                                     className='object-cover w-full'
                                 />
